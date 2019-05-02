c5c09011ecf0ce2d456bc9d0ace0c7db0991d763
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-05-02 11:44:45 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { DataProvider } from '../../providers/data';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+  public data: DataProvider;
+  constructor() {
+    this.data = new DataProvider();
+  }
 }
---
+++ src/providers/data.ts
@@ -0,0 +1,40 @@
+export class DataProvider {
+  public flowers = [
+    {
+      name: 'Tulipe',
+      quantity: 5,
+      color: 'primary',
+      checked: true
+    },
+    {
+      name: 'Marguerite',
+      quantity: 8,
+      color: 'secondary',
+      checked: false
+    },
+    {
+      name: 'Anel',
+      quantity: 10,
+      color: 'danger',
+      checked: true
+    },
+    {
+      name: 'Angèle',
+      quantity: 0,
+      color: 'light',
+      checked: false
+    },
+    {
+      name: 'Patate',
+      quantity: 2,
+      color: 'dark',
+      checked: true
+    },
+    {
+      name: 'Orange',
+      quantity: 1,
+      color: 'secondary',
+      checked: false
+    }
+  ];
+}
